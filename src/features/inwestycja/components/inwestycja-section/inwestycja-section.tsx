83e913b1e3ba4fef194ef3f928d7e118ccc933a8
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2025-07-18 23:59:26 +0200

    fix: correct alignment class in InwestycjaSection component

--- src/features/inwestycja/components/inwestycja-section/inwestycja-section.tsx
+++ src/features/inwestycja/components/inwestycja-section/inwestycja-section.tsx
@@ -13,7 +13,7 @@ export type InwestycjaSectionProps = {
 export const InwestycjaSection = ({ onSubmit }: InwestycjaSectionProps) => {
   return (
     <AccordionItem value="iwestycja-section">
-      <AccordionTrigger className="text-2xl justify-normal align-self-center gap-4">
+      <AccordionTrigger className="text-2xl justify-normal items-center gap-4">
         1. Szczegóły inwestycji
       </AccordionTrigger>
       <AccordionContent className="flex flex-col gap-4 text-balance">
