958362c54cbd991e4cee26c5a74ae7fdac867f9a
Piotr Tomaszek <piotr.tomaszek@capgemini.com>
2025-09-16 14:04:14 +0200

    Save current changes

--- src/features/raport/utils.ts
+++ src/features/raport/utils.ts
@@ -1,12 +1,10 @@
 import { booleanPointInPolygon, point } from '@turf/turf';
 import proj4 from 'proj4';
 
-// Define coordinate systems
 const WGS84 = 'EPSG:4326';
 const EPSG2177 =
   '+proj=tmerc +lat_0=0 +lon_0=18 +k=0.999923 +x_0=6500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
 
-// Function to find zlewnia containing the point using proj4 and turf
 export default async function findZlewnia(coordinates: {
   lat: number;
   lng: number;
@@ -23,7 +21,7 @@ export default async function findZlewnia(coordinates: {
     const testPoint = point(transformedCoords);
 
     for (const feature of geojsonData.features) {
-      // Use Turf.js for accurate point-in-polygon test directly with the feature
+      // Turf.js for accurate point-in-polygon test
       if (booleanPointInPolygon(testPoint, feature)) {
         return {
           nazwaZlewni: feature.properties.nazwa_zlewni,
