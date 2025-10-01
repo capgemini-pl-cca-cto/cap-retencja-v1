5a03b5ffa55063c88961957eae72353a8f532494
Łukasz Łaszkiewicz <lukasz.laszkiewicz@capgemini.com>
2025-10-01 11:50:03 +0200

    feat: move resources to assests

--- src/features/kalkulator/components/kalkulator-form.tsx
+++ src/features/kalkulator/components/kalkulator-form.tsx
@@ -69,7 +69,7 @@ export default function KalkulatorForm({
             Zastanawiasz się, jak przypisać odpowiednie powierzchnie? <br />
             Sprawdź nasz{' '}
             <a
-              href="załącznikgraficzny.pdf"
+              href="assets/załącznikgraficzny.pdf"
               target="_blank"
               rel="noopener noreferrer"
             >
