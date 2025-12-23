97c01a72b4b50589041a7fcdb91ffd0602c4d321
lukasz.laszkiewicz <lukasz.laszkiewicz@capgemini.com>
2025-12-23 11:18:07 +0100

    chore: Add eslint disable comment for exhaustive-deps in useEffect for address search

--- src/features/inwestycja/components/map.tsx
+++ src/features/inwestycja/components/map.tsx
@@ -122,6 +122,7 @@ export default function Map({
         handleMapClick(daneAdresu.lat, daneAdresu.lng);
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [daneAdresu],
   );
 
